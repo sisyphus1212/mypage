8bb10ab2a06e0e4525fbbd48b3553c7ea14dfa92
liaocj <liaocj@yusur.tech>
2024-02-06 18:05:04 +0800

    check time

--- sed_images_path.sh
+++ sed_images_path.sh
@@ -4,6 +4,7 @@ modify_files=$(find "$DIRECTORY" -type f -mmin -60)
 IFS=$'\n'
 for file in $modify_files; do
     if [[ $file == *.md ]]; then
+        stat $file
         sed -i -E 's|\]\((\.\./)+medias/(images_[0-9]+)/([^)]+)\.png\)|\](https://github.com/sisyphus1212/\2/blob/main/\3.png?raw=true)|g' $file
     fi
 done
