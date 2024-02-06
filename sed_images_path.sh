4c9eff7ab63f143aa25c80e1589a912aeb559b2d
liaocj <liaocj@yusur.tech>
2024-02-06 19:46:20 +0800

    fix git add file err

--- sed_images_path.sh
+++ sed_images_path.sh
@@ -5,7 +5,7 @@ echo "============"
 IFS=$'\n'
 for file in "$modify_files"; do
     if [[ $file == *.md ]]; then
-        ls -lh $file
         sed -i -E 's|\]\((\.\./)+medias/(images_[0-9]+)/([^)]+)\.png\)|\](https://github.com/sisyphus1212/\2/blob/main/\3.png?raw=true)|g' $file
+        cat $file
     fi
 done
