bdc9ebaf76b7960080aabcd023a176f68f9463f1
liaocj <liaocj@yusur.tech>
2024-02-06 18:45:28 +0800

    fix err

--- sed_images_path.sh
+++ sed_images_path.sh
@@ -1,6 +1,7 @@
-DIRECTORY="./source"
 modify_files=$(git show --name-only --pretty=format:)
-
+echo "============"
+echo $modify_files
+echo "============"
 IFS=$'\n'
 for file in $modify_files; do
     if [[ $file == *.md ]]; then
