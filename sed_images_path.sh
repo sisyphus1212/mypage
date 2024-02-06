10364ea6d59ea6f5d7b7fdbb8099ca59d8ce0dcb
liaocj <liaocj@yusur.tech>
2024-02-06 19:09:15 +0800

    fix err

--- sed_images_path.sh
+++ sed_images_path.sh
@@ -1,11 +1,11 @@
 git_log_debug=$(git branch)
-modify_files=$(git show --name-only --pretty=format:)
+modify_files="$(git show --name-only --pretty=format:)"
 echo "============"
 echo $git_log_debug
-echo $modify_files
+echo "$modify_files"
 echo "============"
 IFS=$'\n'
-for file in $modify_files; do
+for file in "$modify_files"; do
     if [[ $file == *.md ]]; then
         ls -lh $file
         sed -i -E 's|\]\((\.\./)+medias/(images_[0-9]+)/([^)]+)\.png\)|\](https://github.com/sisyphus1212/\2/blob/main/\3.png?raw=true)|g' $file
