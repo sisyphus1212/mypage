d1ae958f0e5bc42aaba80ae7c79332a50eea71d5
liaocj <liaocj@yusur.tech>
2024-02-06 19:19:30 +0800

    fix git add file err

--- sed_images_path.sh
+++ sed_images_path.sh
@@ -1,5 +1,5 @@
 git_log_debug=$(git branch -v)
-modify_files="$(git show  --stat HEAD^  --name-only   --pretty=format:)"
+modify_files="$(git show --stat HEAD~1 --name-only --pretty=format:)"
 echo "============"
 echo "$git_log_debug"
 echo "$modify_files"
