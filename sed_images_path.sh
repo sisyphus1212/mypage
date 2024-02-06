e16c28c027b9440fc31f834062cbd33628dff436
liaocj <liaocj@yusur.tech>
2024-02-06 18:42:52 +0800

    fix err

--- sed_images_path.sh
+++ sed_images_path.sh
@@ -4,7 +4,7 @@ modify_files=$(git show --name-only --pretty=format:)
 IFS=$'\n'
 for file in $modify_files; do
     if [[ $file == *.md ]]; then
+        ls -lh $file
         sed -i -E 's|\]\((\.\./)+medias/(images_[0-9]+)/([^)]+)\.png\)|\](https://github.com/sisyphus1212/\2/blob/main/\3.png?raw=true)|g' $file
-        cat $file
     fi
 done
