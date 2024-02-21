02342268b8b2404dfeccb4c24efe513691ba96fe
liaocj <liaocj@yusur.tech>
2024-02-21 11:31:20 +0800

    fix err

--- sed_images_path.sh
+++ sed_images_path.sh
@@ -5,9 +5,10 @@ modify_files="$(git show --stat --name-only --pretty=format:)"
 echo "============"
 echo "$modify_files"
 echo "============"
+
 IFS=$'\n'
 for file in "$modify_files"; do
-    if [[ $file == *".md" ]]; then
+    if echo "$string" | grep -q "\.md$"; then
         echo "++++++++++++++"
         sed -i -E 's|\]\((\.\./)+medias/(images_[0-9]+)/([^)]+)\.png\)|\](https://github.com/sisyphus1212/\2/blob/main/\3.png?raw=true)|g' $file
         cat $file
